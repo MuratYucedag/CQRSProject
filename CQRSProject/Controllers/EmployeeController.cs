f3d89eb829726609821cd91f7813990c332f2323
MuratYucedag <yucedagmurat23@gmail.com>
2024-06-06 14:34:02 +0300

    ...

---
+++ CQRSProject/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using CQRSProject.MediatorPattern.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IMediator _mediator;
+        public EmployeeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task<IActionResult> EmployeeList()
+        {
+            var values = await _mediator.Send(new GetEmployeeQuery());
+            return View(values);
+        }
+    }
+}
